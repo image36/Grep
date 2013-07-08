b6298e332112a8b7dad7e998ddc260d7fefb7efe
image36 <barry.d.thomas@gmail.com>
2013-07-08 15:46:46 -0400

    Slimmed down the requirements and made it process just two arguments -path and -pattern

--- Grep.cs
+++ Grep.cs
@@ -2,58 +2,94 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.IO;
-using System.Text.RegularExpressions;
-namespace Grep
+
+namespace Command_line_practice
 {
-                class Program{
-                      private static bool recursive = false;
-                      private static string pattern = string.Empty;
-                      private static string path = string.Empty;
-                                                    
-                      static void Err(string msg)
-                      { 
-                                                                                        
-                      }
-                                                                    
-                      static void Main(string[] args)
-                      {
-                        System.Console.WriteLine(args.Length);
-                        if (args.Length == 0 || args[0] == "-?")
-                        { 
-                            //help message
-                            Console.WriteLine(@"Grep lists files based on matching Regular Expression.
-Arguments:
--?                  Show help message.
--r                  Recursively search files and subdirectories.
--v                  Invert match.
--l                  Suppress normal output and just show file names
--i                  Ignore case.
-");
-                                                                                                                                                                                 
-                                                                                                                                                                                                 
-}
-                                                                                                                                                
-                                                                                                                                                
-}
- static bool parseArg(string arg)
-{
-switch(arg)
-{ 
- //some code to make it recursively check directories
-case "-r":
-recursive = true;
-return true;
-case "-v":
-invertMatch = true;
-return true;
-case "-i":
-ignoreCase = true;
-return true;
-case "-l":
-supressNormalOutput = true;
-return true;
-}
-}
+    class Program
+    {
+        public static string pattern = string.Empty;
+        public static string path = string.Empty;
+        public static string Help = @"This application lists files based on matching Regular Expression.
+                Arguments:
+                -?                  Show help message.
+                -pattern            What to search for.
+                -path               Where to search.
+                ";
+
+
+        static void Main(string[] args)
+        {
+            readArgs(args);
+            System.Console.WriteLine(args.Length);
+            System.Console.WriteLine(@"Your search in {0} for {1} returned the following:       
+****************************************************************************
+****************************************************************************
+            ", path, pattern);
+            SearchDirectory(path, pattern);
+            System.Console.ReadKey();
+            
+
+
+        }
+        private static void readArgs(string[] args)
+        {
+            foreach (string arg in args)
+            {
+
+                if (arg.StartsWith("-pattern"))
+                {
+                    var x = arg.Substring(9);
+                    pattern = x;
+                    //System.Console.WriteLine(pattern);
+
                 }
+                else if (arg.StartsWith("-path"))
+                {
+                    var x = arg.Substring(6);
+                    path = x;
+                    //System.Console.WriteLine(path);
+                }
+                else if (arg.StartsWith("-?"))
+                {
+                    
+                    System.Console.WriteLine(Help);
+                }
+                else
+                {
+                    System.Console.WriteLine( "{0} is not a valid argument", arg);
+                    System.Console.WriteLine(Help);
+                }
+                
+            }
+        }
+        public static void SearchDirectory(string path, string pattern)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+                int results = files.GetLength(0);
+                Console.WriteLine("The total number of files returned is {0}", results);
+                //DirectoryInfo dir = new DirectoryInfo(path, SearchOption.AllDirectories);
+                //int count = dir.GetFiles().Length;
+                //Console.WriteLine("The total number of files in this directory is {0}", count);
+                foreach (string file in files)
+                {
+                    if (results > 1)
+                    {
+                        Console.WriteLine(file);
+                    }
+                    else if (results < 1)
+                    {
+                        Console.WriteLine("There are no results");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed {0}", e.ToString());
+            }
+        }
+    }
 }
