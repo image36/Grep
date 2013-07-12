ed367a8ce211773530a1437c5238377748621972
image36 <barry.d.thomas@gmail.com>
2013-07-11 20:16:22 -0400

    latest

--- Grep.cs
+++ Grep.cs
@@ -4,92 +4,124 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Command_line_practice
 {
     class Program
     {
-        public static string pattern = string.Empty;
-        public static string path = string.Empty;
-        public static string Help = @"This application lists files based on matching Regular Expression.
+        private static string pattern = string.Empty;
+        private static String path = string.Empty;
+        private static bool recursive = false;
+        private static bool supress = false;
+        private static string Help = @"This application lists files based on matching Regular Expression.
                 Arguments:
                 -?                  Show help message.
                 -pattern            What to search for.
                 -path               Where to search.
+                -r or -R            Recursive search
+                -i                  Supress results to show only file names.
                 ";
-
-
         static void Main(string[] args)
         {
             readArgs(args);
-            System.Console.WriteLine(args.Length);
-            System.Console.WriteLine(@"Your search in {0} for {1} returned the following:       
-****************************************************************************
+            DisplayArgs(args);
+            if (pattern == "")
+            {
+                string msg = string.Format("Pattern can not be empty ");
+                Console.Write(msg);
+            }
+            if (pattern != "")
+            {
+                System.Console.WriteLine(@"Your search in {0} for {1} returned the following:       
+Start:**********************************************************************
 ****************************************************************************
             ", path, pattern);
-            SearchDirectory(path, pattern);
+                SearchDirectory(path, pattern, recursive, supress);
+            }
+            System.Console.WriteLine(
+@"End:************************************************************************
+****************************************************************************");
+
             System.Console.ReadKey();
-            
-
-
         }
         private static void readArgs(string[] args)
         {
             foreach (string arg in args)
             {
-
                 if (arg.ToLower().StartsWith("-pattern"))
                 {
                     var x = arg.Substring(9);
                     pattern = x;
-                    //System.Console.WriteLine(pattern);
-
                 }
                 else if (arg.ToLower().StartsWith("-path"))
                 {
                     var x = arg.Substring(6);
                     path = x;
-                    //System.Console.WriteLine(path);
                 }
                 else if (arg.StartsWith("-?"))
                 {
-                    
                     System.Console.WriteLine(Help);
+                }
+                else if (arg.ToLower().StartsWith("-r"))
+                {
+                    recursive = true;
+                }
+                else if (arg.ToLower().StartsWith("-l"))
+                {
+                    supress = true;
                 }
                 else
                 {
-                    System.Console.WriteLine( "{0} is not a valid argument", arg);
+                    System.Console.WriteLine("{0} is not a valid argument", arg);
                     System.Console.WriteLine(Help);
                 }
-                
             }
         }
-        public static void SearchDirectory(string path, string pattern)
+        private static void DisplayArgs(string []args)
         {
+            string msg = string.Format("There were {0} args passed", args.Length);
+            System.Console.WriteLine(msg);
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+        public static void SearchDirectory(string path, string pattern, bool recursive, bool supress)
+        {
+            string[] RecursiveFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            string[] NotRecursiveFiles = Directory.GetFiles(path);
+            string[] files;
             try
             {
-                string[] files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
-                int results = files.GetLength(0);
-                Console.WriteLine("The total number of files returned is {0}", results);
-                //DirectoryInfo dir = new DirectoryInfo(path, SearchOption.AllDirectories);
-                //int count = dir.GetFiles().Length;
-                //Console.WriteLine("The total number of files in this directory is {0}", count);
-                foreach (string file in files)
+                if (recursive == true)
                 {
-                    if (results > 1)
+                    files = RecursiveFiles;
+                }
+                else 
+                {
+                    files = NotRecursiveFiles;
+                }
+                foreach (var f in files)
+                {
+                    var file = new FileInfo(f);
                     {
-                        Console.WriteLine(file);
-                    }
-                    else if (results < 1)
-                    {
-                        Console.WriteLine("There are no results");
+                        if (Regex.IsMatch(file.Name, pattern, RegexOptions.IgnoreCase) && supress == true)
+                        {
+                            Console.WriteLine(file.Name);
+                        }
+                        else if (Regex.IsMatch(file.Name, pattern, RegexOptions.IgnoreCase) && supress == false)
+                        {
+                            Console.WriteLine(file.DirectoryName + "\\" + file.Name);
+                        }
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("The process failed {0}", e.ToString());
+                Console.WriteLine(e);
             }
         }
+
     }
 }
